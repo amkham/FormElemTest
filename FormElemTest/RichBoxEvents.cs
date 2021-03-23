19f0fd6258da8724a36fddf3950ac48b291b22d2
amkham <vspc_vs@VSPC34.RU>
2021-03-23 09:33:24 +0300

    Добавьте файлы проекта.

---
+++ FormElemTest/RichBoxEvents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormElemTest
+{
+    public static class RichBoxEvents
+    {
+
+        // target - целевое слово для замены
+        // fileLocation - путь к файлу на диске
+        // color - цвет 
+        // start - начальная позиция поиска
+        static public void FindAndChangeColor(string target, string fileLocation, Color color, int start)
+        {
+            RichTextBox textBox = new RichTextBox();
+            textBox.LoadFile(fileLocation);
+            int newPosition = textBox.Find(target, start + target.Length, RichTextBoxFinds.MatchCase);
+            textBox.SelectionColor = color;
+            textBox.SaveFile(fileLocation, RichTextBoxStreamType.RichText);
+
+            if (newPosition != -1)
+            {
+                FindAndChangeColor(target, fileLocation, color, newPosition);
+
+            }
+        }
+
+
+        // start, end - границы поиска
+        static public void FindAndChangeColor(string target, string fileLocation, Color color, int start, int end)
+        {
+            
+        }
+
+
+        // sample - замена 
+        static public void FindAndChangeWord(string target, string sample, string fileLocation, int start, int end)
+        {
+
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FindAndChangeColorTest()
+        {
+
+          
+        }
+    }
+}
